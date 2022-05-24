3d7c64b1ceb69bfd86d38f92265ac96b312f0597
Julian Henao DMS <julian.henao@dms.ms>
2022-05-24 17:21:28 -0500

    Añade ngModel a campos del formulario y define datos en el constructor del archivo ts del componente formato-peritaje

--- src/app/components/formato-peritaje/formato-peritaje.component.ts
+++ src/app/components/formato-peritaje/formato-peritaje.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsVehiculosUsados } from './interfaces/items-vehiculo';
 import { Vehiculo } from './interfaces/vehiculo';
 
 interface CalificacionesEstado {
@@ -29,7 +30,7 @@ export class FormatoPeritajeComponent implements OnInit {
   public tela = 0;
   public cuero = 1;
  
-  public selectedValue: string | undefined;
+  // public selectedValue: string | undefined;
   public selectedCombustible: string | undefined;
  
   calificaciones: CalificacionesEstado[] = [
@@ -48,7 +49,7 @@ export class FormatoPeritajeComponent implements OnInit {
    {value: '4', viewValue: 'Eléctrico'},
   
  ];
- 
+ public formularioVehiculos : ItemsVehiculosUsados;
  public infoVehiculo : Vehiculo;
    constructor() { 
      this.infoVehiculo = {
@@ -61,6 +62,55 @@ export class FormatoPeritajeComponent implements OnInit {
        Prueba_ruta : 0,
        Fecfin : new Date,
        Califica2 : 0,
+     }
+     this.formularioVehiculos = {
+      id_veh_chk_usados: 0,
+      califica: '',
+      fecini: new Date,
+      id_usuario: 0,
+      id_usu_inspector: 0,
+      id_cot_item_lote: 0,
+      prueba_ruta: 0,
+      fecfin: new Date,
+      califica2: 0,
+      id_veh_chk_usados_mas: 0,
+      llave: 0,
+      libro: 0,
+      cambio_correa: 0,
+      lugar_cambio_correa: '',
+      herramienta: 0,
+      manual: 0,
+      reclamo_aseg: 0,
+      valor_reclamo_aseg: 0,
+      marca_radio: '',
+      marca_llantas: '',
+      marca_bateria: '',
+      lugar_mante: '',
+      cilindraje: '',
+      color: '',
+      referencia: '',
+      clase: 0, // Que es Clase?
+      combustible: 0,
+      lugar_matricula: 0,
+      capacidad_sillas: 0,
+      km: 0,
+      fec_prox_mantenimiento: new Date,
+      cojineria: 0,
+      caja: 0,
+      traccion: 0,
+      rin: 0,
+      estribo: 0,
+      vidrios: 0,
+      techo_vidrio: 0,
+      retrovisores_electricos: 0,
+      no_airbags: 0,
+      no_rin: 0,
+      barra_techo: 0,
+      exploradora: 0,
+      placa: '',
+      linea: '',
+      marca: '',
+      modelo: '',
      }
    }
  
