a83cf23b45c94f0997deedf7fe8d90a06a329030
Julian Henao DMS <julian.henao@dms.ms>
2022-05-25 09:26:52 -0500

    Crea interfaces para estado pintura y elementos Az. Crea archivo de servicios para iniciar implementación del API

---
+++ src/app/components/formato-peritaje/interfaces/elementos-az.ts
@@ -0,0 +1,9 @@
+export interface ElementosAz{
+    id: number,
+    id_veh_chk_usados: number,
+    id_chk_maestro_elementos: number,
+    elemento: string,
+    est: number,
+    intervencion: number,
+    valor: number
+}
---
+++ src/app/components/formato-peritaje/interfaces/estado-pintura.ts
@@ -0,0 +1,14 @@
+export interface EstadoPintura{
+    id: number,
+    id_veh_chk_usados: number,
+    id_chk_maestro_partes: number,
+    parte: string,
+    id_chk_estado: number,
+    estado: string,
+    repi_tipo_a: number,
+    repi_tipo_b: number,
+    repar_tipo_a: number,
+    repar_tipo_b: number,
+    cambiada: number,
+    removida: number
+}
