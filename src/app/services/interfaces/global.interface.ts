8dae1518227bfe2d307cf0dd654a5879fa8d9ba0
Camilo Ballesteros Yepes <camilo.ballesteros@dms.ms>
2022-06-04 17:49:52 -0500

    Agregado variable global , cargados items de ormulario nuevo y de editar formulario

--- src/app/services/interfaces/global.interface.ts
+++ src/app/services/interfaces/global.interface.ts
@@ -1,8 +1,10 @@
 import { Encabezados } from "../../components/formato-peritaje/interfaces/encabezados.interface";
+import { EstadoPintura } from "../../components/formato-peritaje/interfaces/estado-pintura";
 import { formulario } from "../../components/formato-peritaje/interfaces/formulario.interface";
 
 export interface Global {
   isEmpty: boolean,
   formulario: formulario,
-  encabezados: Encabezados
+  encabezados: Encabezados,
+  listaPartes: EstadoPintura[]
 }
