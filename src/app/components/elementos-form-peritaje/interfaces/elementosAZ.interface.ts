2ceaaaa88183f681d23c2b3c9c57d48dd5d0fccd
Camilo Ballesteros Yepes <camilo.ballesteros@dms.ms>
2022-06-08 18:05:48 -0500

    cambios componente elementos, guardado correcto de formulario para editar, correcion a guardado incorrecto de intervenciones de elementos AZ

---
+++ src/app/components/elementos-form-peritaje/interfaces/elementosAZ.interface.ts
@@ -0,0 +1,11 @@
+export interface ElementosAz{
+  id: number,
+  id_veh_chk_usados: number,
+  id_chk_maestro_elementos: number,
+  elemento: string,
+  est: number,
+  intervencion: number,
+  valor: number,
+  valor_def: number,
+  accion?: number
+}
---
+++ src/app/components/elementos-form-peritaje/interfaces/estadoPintura.interface.ts
@@ -0,0 +1,19 @@
+export interface EstadoPintura{
+    id: number,
+    id_veh_chk_usados: number,
+    id_chk_maestro_partes: number,
+    parte: string,
+    id_chk_estado: number,
+    estado: string,
+    repi_tipo_a: number,
+    repi_tipo_b: number,
+    repar_tipo_a: number,
+    repar_tipo_b: number,
+    cambiada: number,
+  removida: number,
+  valor_def: number,
+  repi_tipo: number,
+  repar_tipo: number,
+  estadoParte: number,
+  accion?: number
+}
